d9fa8311597571ddf1e4a0b8642c0c2d89f45d33
Steve Meyer <stephen.meyer@wisc.edu>
2021-04-27 22:24:08 -0500

    Implement the quadrant graph for parsed topics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import * as d3 from "d3";
+import { complex, add, multiply, chain, sqrt } from "mathjs";
 import { TopicModeler } from "topical";
 import "./style.css";
+
+
+const width  = 700,
+      height = 700,
+      margin = {top: 30, bottom: 50, left: 50, right: 50},
+      constants = {radii: [0, 15], rotationCoefficient: complex((1 / sqrt(2)), (1 / sqrt(2)))},
+      svg = d3.select("svg").attr("width", width).attr("height", height);
 
 
 function process(event) {
@@ -58,6 +66,132 @@ function process(event) {
 
       let finish = new Date();
       document.getElementById("time").innerHTML = (finish - start) + "ms";
+
+
+      let topicLabels = topicTopWords.map((item, number) => modeler.topNWords(modeler.topicWordCounts[number], 1));
+
+      modeler.documents.forEach(doc => {
+        const topic1 = multiply(constants.rotationCoefficient, complex(doc.topicCounts[0], 0));
+        const topic2 = multiply(constants.rotationCoefficient, complex(0, doc.topicCounts[1]));
+        const topic3 = multiply(constants.rotationCoefficient, complex(-doc.topicCounts[2], 0));
+        const topic4 = multiply(constants.rotationCoefficient, complex(0, -doc.topicCounts[3]));
+
+        doc.coordinates = chain(topic1)
+                            .add(topic2)
+                            .add(topic3)
+                            .add(topic4)
+                            .done();
+      });
+
+
+      const xScale = d3.scaleLinear()
+        .domain(d3.extent(modeler.documents, d => d.coordinates.re))
+        .range([margin.left, width - margin.right])
+
+
+      const yScale = d3.scaleLinear()
+        .domain(d3.extent(modeler.documents, d => d.coordinates.im))
+        .range([height - margin.bottom, margin.top])
+
+
+      // TODO: pick a real data point for the color
+      const colorScale = d3.scaleSequential(d3.interpolateBuPu)
+        .domain(d3.extent(modeler.documents, d => d.coordinates.re))
+
+
+      // TODO: pick a real data point for the color
+      const sizeScale = d3.scaleSqrt()
+        .domain(d3.extent(modeler.documents, d => d.coordinates.re))
+        .range(constants.radii)
+
+
+      const xAxis = d3.axisBottom(xScale);
+      const yAxis = d3.axisLeft(yScale);
+
+
+      const dotsContainer = svg.append("g").attr("class","container")
+
+      const dotgroups = dotsContainer.selectAll(".dot")
+        .data(modeler.documents)
+        .join("g")
+        .attr("class", "dot")
+        .attr("transform", d => `translate(${xScale(d.coordinates.re)}, ${yScale(d.coordinates.im)})`);
+
+      dotgroups.append("circle")
+        .attr("fill", "lightgrey")
+        .attr("opacity", 0.6)
+        // TODO
+        .attr("r", "5");
+
+      svg.append("g")
+        .attr("class", "axis x-axis")
+        .attr("transform", `translate(0, ${height - margin.bottom})`)
+        .call(xAxis)
+        .append("text")
+        .attr("class", "label")
+        .text("X Axis")
+        .attr("dy", "3em")
+        .attr("x", "50%");
+
+      svg.append("g")
+        .attr("class", "axis y-axis")
+        .attr("transform", `translate(${margin.left}, 0)`)
+        .call(yAxis)
+        .append("text")
+        .attr("class", "label")
+        .text("Y Axis")
+        .attr("y", "50%")
+        .attr("dx", "-3em");
+
+      // add quadrant lines
+      const annotationGroup = svg.append("g").attr("class", "annotations")
+
+      annotationGroup.append("path")
+        .attr("stroke","grey")
+        .attr("transform",`translate(${xScale(0)},0)`)
+        .attr("d", `M 0 ${margin.top} L 0 ${height - margin.bottom}`)
+
+      annotationGroup.append("path")
+        .attr("stroke","grey")
+        .attr("transform",`translate(0,${yScale(0)})`)
+        .attr("d", `M ${margin.left} 0  L ${width - margin.right} 0`)
+
+      annotationGroup.append("text")
+        .attr("x", width - margin.right)
+        .attr("y", margin.top * 2)
+        .style("text-anchor", "end")
+        .text("Q1: " + topicLabels[0]);
+
+      annotationGroup.append("text")
+        .attr("x", margin.left * 1.5)
+        .attr("y", margin.top * 2)
+        .style("text-anchor", "start")
+        .text("Q2: " + topicLabels[1]);
+
+      annotationGroup.append("text")
+        .attr("x", margin.left * 1.5)
+        .attr("y", height * .9)
+        .style("text-anchor", "start")
+        .text("Q3: " + topicLabels[2]);
+
+      annotationGroup.append("text")
+        .attr("x", width - margin.right)
+        .attr("y", height * .9)
+        .style("text-anchor", "end")
+        .text("Q4: " + topicLabels[3]);
+
+      let articles = d3.select("#graph")
+          .selectAll(".article")
+          .data(modeler.documents)
+        .enter()
+          .append("div")
+          .attr("class", "article");
+
+      articles.append("p").text(d => d.originalText);
+      articles.append("p").text(d => {
+        return topicTopWords.map((item, number) => `${topicLabels[number]}: ${d.topicCounts[number]}`).join("; ");
+      });
+      articles.append("p").text(d => d.coordinates);
     });
   }
 }
